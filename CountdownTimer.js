6709db69bfff337a8953732f92381d7c35474ba2
Aman Chaturvedi <62074888+aman774554@users.noreply.github.com>
2024-07-07 18:25:48 +0530

    Update CountdownTimer.js

--- CountdownTimer.js
+++ CountdownTimer.js
@@ -1,1 +1,37 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [seconds, setSeconds] = useState(10);
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (active && seconds > 0) {
+      interval = setInterval(() => {
+        setSeconds((prev) => prev - 1);
+      }, 1000);
+    } else if (seconds === 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [active, seconds]);
+
+  return (
+    <div className="App">
+      <h1>{seconds}</h1>
+      <button onClick={() => setActive(!active)}>
+        {active ? "Pause" : "Start"}
+      </button>
+      <button
+        onClick={() => {
+          setActive(false);
+          setSeconds(10);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
 
