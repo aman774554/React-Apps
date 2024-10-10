7a6544944b25fba4bf1089d2073da57bbf879768
Aman Chaturvedi <62074888+aman774554@users.noreply.github.com>
2024-10-10 17:25:42 +0530

    Update coding-exercise.js

--- coding-exercise.js
+++ coding-exercise.js
@@ -12,11 +12,11 @@ function removeDuplicateCharacters() {
 console.log(removeDuplicateCharacters());
 
 //OR 
-
+-----------------------------------
  console.log([... new Set(string)].join(""))
 
 //OR (in - index, of - word)
-
+----------------------------------
 var string='priya riya supriya'
   arr = [];
     for(let i of string){
@@ -32,3 +32,14 @@ var arr = [55, 44, 55,67,67,67,67,8,8,8,8,8,65,1,2,3,3,34,5];
 var unique = [...new Set(arr)]
 console.log(unique) //output: [55, 44, 67, 8, 65, 1, 2, 3, 34, 5]
 console.log(unique.length) //output: 10
+
+================================================================================================================================================================================
+Code 4:String reverse without reversing of individual words (Array of elements can be reverse with reverse() method but for string it is won't possible so required to split 
+and then join().
+
+function removeDuplicates(){
+   var string ="India is my country"
+  let result = string.split('').reverse().join('');
+  return result;
+}
+console.log(removeDuplicates()) 
