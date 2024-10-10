ef916c507882e58d87e26337c6bae9888f2e6cf5
Aman Chaturvedi <62074888+aman774554@users.noreply.github.com>
2024-10-10 17:21:20 +0530

    Update coding-exercise.js

--- coding-exercise.js
+++ coding-exercise.js
@@ -1,6 +1,7 @@
 Mutability: Primitive types are immutable, while non-primitive types are mutable.
 ================================================================================================================================================================================
 Code 1: Remove Duplicate characters from String
+
 function removeDuplicateCharacters() {
   var string='priya riya supriya'
  return Array.from(string).filter((data,index)=>{
@@ -11,9 +12,11 @@ function removeDuplicateCharacters() {
 console.log(removeDuplicateCharacters());
 
 //OR 
+
  console.log([... new Set(string)].join(""))
 
 //OR (in - index, of - word)
+
 var string='priya riya supriya'
   arr = [];
     for(let i of string){
@@ -22,3 +25,10 @@ var string='priya riya supriya'
         }
     }
     console.log(arr.join(""))
+      
+================================================================================================================================================================================
+Code 2: Remove Duplicate characters from array of element and find the count of an elements using set object
+var arr = [55, 44, 55,67,67,67,67,8,8,8,8,8,65,1,2,3,3,34,5];
+var unique = [...new Set(arr)]
+console.log(unique) //output: [55, 44, 67, 8, 65, 1, 2, 3, 34, 5]
+console.log(unique.length) //output: 10
