e2a24ee6a1cc7c958a03066dbf83d411d31ec85a
Aman Chaturvedi <62074888+aman774554@users.noreply.github.com>
2025-04-13 21:22:20 +0530

    Update generateRandomString.js

--- generateRandomString.js
+++ generateRandomString.js
@@ -1,3 +1,5 @@
+//Math.floor(Math.random() * (max - min + 1)) + min;
+
 function generateRandomString(length = 6) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
