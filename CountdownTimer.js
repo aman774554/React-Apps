4d29918e95de0a6b55b05c2eff6c6b90df885844
Aman Chaturvedi <62074888+aman774554@users.noreply.github.com>
2026-06-13 23:54:22 +0530

    Update CountdownTimer.js

--- CountdownTimer.js
+++ CountdownTimer.js
@@ -1,13 +1,41 @@
+==================================================================
+  export default function App() {
+  const [count, setCount] = useState(3);
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+    let interval;
+    if (count === 0) return;
+    if (active && count > 0) {
+      interval = setInterval(() => {
+        setCount((prev) => prev - 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [active, count]);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setActive(!active)}>
+        {!active ? 'Start' : 'Pause'}
+      </button>
+      <button
+        onClick={() => {
+          setActive(false);
+          setCount(4);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
+====================================================================================================
 import { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
   const [seconds, setSeconds] = useState(10);
   const [active, setActive] = useState(false);
-
-  // In React Strict Mode development builds, React mounts, unmounts, and mounts again to expose bugs like missing cleanup functions. 
-  // Without clearInterval, you end up with multiple intervals running simultaneously
-
   useEffect(() => {
     let interval = null;
     if (active && seconds > 0) {
@@ -42,6 +70,8 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 export function App(props) {
   const [second, setSecond] = useState(3);
+  // In React Strict Mode development builds, React mounts, unmounts, and mounts again to expose bugs like missing cleanup functions. 
+  // Without clearInterval, you end up with multiple intervals running simultaneously
   useEffect(() => {
     if (second === 0) return;
       var interval = setInterval(() => {
